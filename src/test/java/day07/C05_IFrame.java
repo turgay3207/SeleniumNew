5de5f4a129ef7e09f218d80485504e51c68cc1ae
Turgay Uludag <uludagturgay32@gmail.com>
2022-12-30 22:00:58 +0300

    newt1

--- src/test/java/day07/C05_IFrame.java
+++ src/test/java/day07/C05_IFrame.java
@@ -8,14 +8,37 @@ import org.openqa.selenium.By;
 public class C05_IFrame extends TestBase01{
 
     @Test
-    public void iframeTest() throws InterruptedException {
+    public void iframeTest(){
         driver.get("https://testcenter.techproeducation.com/index.php?page=iframe");
-      String actualTitle=  driver.findElement(By.xpath("//p[normalize-space()='An iframe with a thin black border:']")).getText();
-      String exceptTitle="An iframe with a thin black border:";
-        Assert.assertEquals(exceptTitle,actualTitle);
+//        Ana sayfadaki ‘An iframe with a thin black border:’ metninde ‘black border’ yazisinin oldugunu test edelim
+        String anaMetin = driver.findElement(By.xpath("//p[.='An iframe with a thin black border:']")).getText();
+//        //tag[.='metin']; Bana bu tagli metni ver
+        //tag[text()='metin'];
+        String expectedText="black border";
+        Assert.assertTrue(anaMetin.contains(expectedText));
+//        Ayrica ‘Applications lists’ yazisinin sayfada oldugunu test edelim
+        //Applications lists elementi iframe in icinde. Iframe switch yapmam sart
+        driver.switchTo().frame(0);//index 0 dan baslar. ilk iframe e switch yapiyoruz
+
+        String icMetin = driver.findElement(By.xpath("//*[.='Applications lists']")).getText();
+        String expectedText1="Applications lists";
+        Assert.assertEquals(expectedText1,icMetin);
+//        Alternatif olarak altdaki secenekler de kullanilabilir
+//        Assert.assertTrue(expectedText1.contains(icMetin));
+//        Assert.assertTrue(driver.findElement(By.xpath("//*[.='Applications lists']")).isDisplayed());
+//        Son olarak footer daki (Anasayfadaki) ‘Povered By’ yazisini varligini test edilip
+        driver.switchTo().defaultContent();
+
+       String beklenen= driver.findElement(By.xpath("//footer[@class='blog-footer']//p[1]")).getText();
+        System.out.println(beklenen);
+       String girilen="Povered By";
+
+       Assert.assertTrue(beklenen.contains(girilen));
 
 
-        Thread.sleep(3000);
 
     }
+
+
+
 }
